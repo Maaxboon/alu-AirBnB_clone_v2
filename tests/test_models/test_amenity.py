6345cd8959902c6c5fdeeab342661d99813acd40
Maaxboon <maaxwelokoth@gmail.com>
2025-01-22 19:13:45 +0300

    Improvements

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,19 +1,21 @@
 #!/usr/bin/python3
-""" """
-from tests.test_models.test_base_model import test_basemodel
+"""
+Unit tests for the Amenity class.
+"""
+from tests.test_models.test_base_model import TestBaseModel
 from models.amenity import Amenity
 
 
-class test_Amenity(test_basemodel):
-    """ """
+class TestAmenity(TestBaseModel):
+    """Tests for Amenity class."""
 
     def __init__(self, *args, **kwargs):
-        """ """
+        """Initialize test class."""
         super().__init__(*args, **kwargs)
         self.name = "Amenity"
         self.value = Amenity
 
     def test_name2(self):
-        """ """
+        """Test if the name attribute is of type str."""
         new = self.value()
         self.assertEqual(type(new.name), str)
